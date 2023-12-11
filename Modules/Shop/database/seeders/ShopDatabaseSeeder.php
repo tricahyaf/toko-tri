ea6cc393a3d1cb4b307a51f712cb224dc677023b
Tri Cahya Fitriansyah <tricahya644@gmail.com>
2023-12-11 11:45:07 +0700

    first commit

---
+++ Modules/Shop/database/seeders/ShopDatabaseSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Modules\Shop\database\seeders;
+
+use Illuminate\Database\Seeder;
+
+class ShopDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+        $this->call(ProductSeeder::class);
+    }
+}
---
+++ resources/views/themes/toko-tri/home.blade.php
@@ -0,0 +1,5 @@
+@extends('themes.toko-tri.layouts.app')
+
+@section('content')
+<h1>Hello from home blade</h1>
+@endsection
